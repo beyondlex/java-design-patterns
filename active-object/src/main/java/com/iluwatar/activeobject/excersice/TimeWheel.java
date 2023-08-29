0863fcb95e6ff7521172ce1a4fe300b1b9ffb0e3
Doge <doge@home.com>
2023-08-29 23:51:21 +0800

    Timewheel: refactor

--- active-object/src/main/java/com/iluwatar/activeobject/excersice/TimeWheel.java
+++ active-object/src/main/java/com/iluwatar/activeobject/excersice/TimeWheel.java
@@ -6,27 +6,105 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class TimeWheel {
 
+    int totalRound;
+    List<Runner> runners;
+
+    Runner z = new Runner("Z", 100, Position.Z);
+
+    boolean end = false;
+
+    Runner current = null;
+
+    public TimeWheel(List<Runner> runners, int totalRound) {
+        if (null == runners || runners.size() == 0) {
+            throw new RuntimeException("Please add at least one runner");
+        }
+        this.totalRound = totalRound;
+        this.runners = runners;
+        this.runners.add(z);
+    }
+
+    public int currentRound() {
+        return z.round;
+    }
+
+    public Runner currentRunner() {
+        return current;
+    }
+
+    public void setEnd() {
+        end = true;
+    }
+    public boolean end() {
+        return z.round > totalRound;
+    }
+
+    public Runner next() {
+        if (null != current) {
+            // todo: if current is freeze, keep current round? or just next round
+            this.current.roundNext();
+        }
+        if (end()) {
+            return null; // END
+        }
+        Runner theOne = this.runners.stream().min((me, others) -> {
+            float myLen = length * me.round;
+            float othersLen = length * others.round;
+            if (myLen / me.speed > othersLen / others.speed) {
+                return 1;
+            } else if (myLen / me.speed < othersLen / others.speed) {
+                return -1;
+            }
+            // if speed is the same, compare with position
+            if (me.position.num < others.position.num) {
+                return -1;
+            } else if (me.position.num > others.position.num) {
+                return 1;
+            }
+            return 0;
+        }).get();
+
+        this.current = theOne;
+
+        // theOne.roundNext();
+
+        if (z.round > totalRound) {
+            setEnd();
+        }
+        return theOne;
+    }
+
     @Getter
     @Setter
-    @AllArgsConstructor
     @ToString
     public static class Runner {
+        // todo: generic
+        public Runner(String name, float speed, Position position) {
+            this.name = name;
+            this.speed = speed;
+            this.position = position;
+            this.round = 1;
+        }
         String name;
         float speed;
-        int round;
-
         Position position;
+        int round;
 
         public void roundNext() {
             this.round++;
         }
     }
-    static int length = 1000; // todo: per round?
+
+    /**
+     * length per round
+     */
+    static int length = 1000;
 
     /*
     i.speed = 100
@@ -52,13 +130,37 @@ public class TimeWheel {
 
      */
     public static void main(String[] args) {
-        Runner a = new Runner("A", 120, 1, Position.ONE);
-        Runner b = new Runner("B", 150, 1, Position.TWO);
-        Runner c = new Runner("C", 200, 1, Position.THREE);
-        Runner d = new Runner("D", 150, 1, Position.FOUR);
-        Runner e = new Runner("E", 100, 1, Position.FIVE);
+        Runner a = new Runner("A", 120, Position.ONE);
+        Runner b = new Runner("B", 150, Position.TWO);
+        Runner c = new Runner("C", 200, Position.THREE);
+        Runner d = new Runner("D", 150, Position.FOUR);
+        Runner e = new Runner("E", 100, Position.FIVE);
+        List<Runner> runners = new ArrayList<>();
+        runners.add(a);
+        runners.add(b);
+        runners.add(c);
+        runners.add(d);
+        runners.add(e);
+        TimeWheel timeWheel = new TimeWheel(runners, 10);
+        while(!timeWheel.end()) {
+            Runner theOne = timeWheel.next();
+            if (theOne == null) {
+                System.out.println("END-----------");
+                break;
+            }
+            System.out.printf("(%s): %s", timeWheel.currentRound(), theOne);
+            System.out.printf("\t%s %n", length*theOne.round/theOne.speed);
+        }
+    }
 
-        Runner z = new Runner("Z", 100, 1, Position.Z);
+    public static void main0(String[] args) {
+        Runner a = new Runner("A", 120, Position.ONE);
+        Runner b = new Runner("B", 150, Position.TWO);
+        Runner c = new Runner("C", 200, Position.THREE);
+        Runner d = new Runner("D", 150, Position.FOUR);
+        Runner e = new Runner("E", 100, Position.FIVE);
+
+        Runner z = new Runner("Z", 100, Position.Z);
 
         int totalRound = 4;
         System.out.printf("Round %s begin: %n", z.getRound());
